449e2554562e982f4281b9d58ec8c7f2e9906070
Gabriela Gonzalez <anagabrielagonzalezteran@gmail.com>
2022-11-23 18:08:54 +0100

    responsive form contact

--- src/components/formularios/form_contacto/formulario/FormContacto.jsx
+++ src/components/formularios/form_contacto/formulario/FormContacto.jsx
@@ -4,27 +4,32 @@ import "./form_contacto.css";
 function FormContacto() {
   return ( 
     
-    <div className="container_fcontacto">
-    <form action="" className="form">
-        <div className="form-input">
-            <label for="email">Correo</label>
-            <input type="text" id="email"/>
-        </div> 
-        <br></br>
-        <br></br>
+    <div className="container_fcontact">
+
         
-        <div className="form-input">
-            <label for="mensaje">Mensaje</label>
-            <textarea name="mensaje" id="mensaje"></textarea>
+        <form action="" className="formContact">
+            <div className="inputContact">
+
+                <label for="email">Correo electrónico</label>
+                <input type="text" id="email"/>  
+
+                <label for="email">Teléfono</label>
+                <input type="num" id="phone"/>          
+            
+                <label for="mensaje">Mensaje</label>
+                <textarea name="mensaje" id="mensaje"></textarea>
             </div>
-        <div>
-            <input type="checkbox" id="termsAndConditions"/>
-            <label for="termsAndConditions">Acepto política de privacidad de ADG</label>
-        </div>
-        <div className="form-button">
-            <input type="submit" value="Contáctanos"/>
-        </div>
-    </form>
+                <div className='pv'>
+                    <input type="checkbox" id="termsAndConditions"/>
+                    <label for="termsAndConditions">Acepto política de privacidad de ADG</label>
+                </div>
+                
+            
+
+            <div className="buttonContact">
+                <button>Enviar</button>
+            </div>
+        </form>
 </div>
   )
 }
