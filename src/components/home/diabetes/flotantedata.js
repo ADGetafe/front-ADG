7ac359ea9bd9530308af0157fbb7ead4c019fb66
Gabriela Gonzalez <anagabrielagonzalezteran@gmail.com>
2022-11-24 02:12:16 +0100

    cambios en modal

--- src/components/home/diabetes/flotantedata.js
+++ src/components/home/diabetes/flotantedata.js
@@ -3,25 +3,13 @@
 const Definiciondata = [
     {
       id: 1,
-      titulo: "Definición",
-      texto: "",
+      titulo1: "Diabetes",
+      titulo2: "Tipos de diabetes",
+      parrafo1: "La diabetes tipo 1 es una enfermedad autoinmune crónica que se caracteriza porque el páncreas pierde la capacidad de generar insulina, la hormona que regula la hiperglucemia. Por este motivo, las personas con diabetes tipo 1 requerirán la administración de insulina a diario de por vida. Entre el 5% y el 10% de las personas con  diabetes en el mundo padecen este tipo de la enfermedad. Los motivos detrás de esta respuesta autoinmune aún no se conocen del todo. Parece que existe cierto componente genético en la predisposición a desarrollarla, pero también puede que sea necesario un componente ambiental para que se desencadene la reacción inmune que acabará destruyendo las células beta de los islotes de Langerhans, que son las regiones del páncreas encargadas de producir las hormonas que regularán la cantidad de glucosa en la sangre. El debut de la diabetes tipo 1 se suele producir a edades tempranas, durante la infancia o la adolescencia, por lo que esta enfermedad también se conoce como diabetes infantil oinfanto-juvenil. En algunos casos minoritarios la reacción autoinmune puede producirse ya en la edad adulta, lo que dificulta su diagnóstico; existen pruebas específicas para evalua la capacidad de producción de insulina del páncreas o si se ha producido una autoinmune contra la hormona."
       
       
     },
-    {
-      id: 2,
-      desc: "La diabetes es una enfermedad crónica ",
-      title: "Prevención",
-      cover: "",
-      
-    },
-    {
-      id: 3,
-      desc: "¿Qué es lo que no se le aconseja comer un diabético?",
-      title: "Vida saludable",
-      cover: "",
-      
-    },
+
   ]
  
 
