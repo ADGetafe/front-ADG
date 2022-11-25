a701b0c9ec18596ab6cfd6d7d9ad2d2996e237a7
Alexandra Zambrano <alezambrano03@gmail.com>
2022-11-25 09:57:38 +0100

    changes

--- src/components/home/diabetes/Flotante.jsx
+++ src/components/home/diabetes/Flotante.jsx
@@ -6,71 +6,67 @@ import KeyboardDoubleArrowRightIcon from "@mui/icons-material/KeyboardDoubleArro
 import Definiciondata from './flotantedata';
 
 const useStyles = makeStyles((theme) => ({
-    iconos:{
-      cursor: 'pointer'
-    }, 
-    inputMaterial:{
-      width: '100%'
-    }
-    
-  }));
+iconos:{
+cursor: 'pointer'
+},
+inputMaterial:{
+width: '100%'
+}
+
+}));
 
 function Flotante() {
-    const styles= useStyles();
-      const [modalInsertar, setModalInsertar]=useState(false);
-  const abrirCerrarModalInsertar=()=>{
-    setModalInsertar(!modalInsertar);
-  }
+const styles= useStyles();
+const [modalInsertar, setModalInsertar]=useState(false);
+const abrirCerrarModalInsertar=()=>{
+setModalInsertar(!modalInsertar);
+}
 
-  const bodyInsertar = (
+const bodyInsertar = (
 
-    
-    <div className={styles.modal}>  
 
-        
-        
+<div className={styles.modal}>
 
-            <div className='container-flotante'>
-              <div className='titleCloseBtn'>
-                <Button onClick={()=>abrirCerrarModalInsertar()}>X</Button>
-              </div>
-              {Definiciondata.map((def) => {
-          return (
-            <>
-              <div className='heading-modal'>
-                  <h1>{def.titulo1}</h1>
-              </div>
 
-              <div className="body-modal">                  
-                   <p>{def.parrafo1}</p>
-                   <p>{def.parrafo2}</p>
-                   <p>{def.parrafo3}</p>
-                   <p>{def.parrafo4}</p>
-                   <p>{def.parrafo5}</p>
-
-              </div>
-            </>
-          )
-        })
-      }
-        </div>
+  <div className='container-flotante'>
+    <div className='titleCloseBtn'>
+      <Button onClick={()=>abrirCerrarModalInsertar()}>X</Button>
     </div>
-  
-  )
+    {Definiciondata.map((def) => {
+    return (
+    <>
+      <div className='heading-modal'>
+        <h1>{def.titulo1}</h1>
+      </div>
+
+      <div className="body-modal">
+        <p>{def.parrafo1}</p>
+        <p>{def.parrafo2}</p>
+        <p>{def.parrafo3}</p>
+        <p>{def.parrafo4}</p>
+        <p>{def.parrafo5}</p>
+      </div>
+    </>
+    )
+    })
+    }
+  </div>
+</div>
+
+)
 
 
-  return (
-    <div className="flotante">
-        <Button onClick={()=>abrirCerrarModalInsertar()}>
-        Leer más <KeyboardDoubleArrowRightIcon className='icon-diabete' />
-        </Button>
-          <Modal
-          open={modalInsertar}
-          onClose={abrirCerrarModalInsertar}>
-              {bodyInsertar}
-          </Modal>
-    </div>
-  )
+return (
+<div className="flotante">
+  <Button onClick={()=>abrirCerrarModalInsertar()}>
+    Leer más
+    <KeyboardDoubleArrowRightIcon className='icon-diabete' />
+  </Button>
+  <Modal open={modalInsertar} onClose={abrirCerrarModalInsertar}>
+    {bodyInsertar}
+  </Modal>
+</div>
+)
 }
 
 export default Flotante
